f64bedc4b2455867bc8d0f518b1161d2e7e2d695
THACHER <sophie.thacher@gmail.com>
2016-06-29 18:07:52 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Track Everything Else
+This is the server portion of the Track Everything Else App.
+
+##Getting Started
+To get started developing, setup the DB first here X
+
+To start the server: ```npm start```
+
+The client will be here X
--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var users = require('./users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({ title: 'Express' });
 });
-
+router.use('/users', users);
 module.exports = router;
